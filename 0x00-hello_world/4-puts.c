d64701c0b42ae02bf0d6d73b62ee46c9699a7b3c
Eze Peter Njoku <holypeter01@gmail.com>
2023-08-13 17:35:29 -0700

    gcc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include# <stdio.h>
 /**
- *main - entry point 
- *return: always 0 (success)
+ * main - a programme that print 
+ * return: 0 (succezs)
  */
-int main(void)
+int main(voi)
 {
-puts(*\"Programming is like building a multilingual puzzle,\n");
+puts("\*"Programming is like building a multilingual puzzle")
 return (0);
 }
-
